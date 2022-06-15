b734722e3a448dce3dd49ac63ac9d95f14278f0b
Kim min seop <93521131+MIN-SEOP-KIM@users.noreply.github.com>
2022-06-15 23:00:00 +0900

    [Bronze II] Title: 나누기, Time: 228 ms, Memory: 18292 KB -BaekjoonHub

---
+++ 백준/Bronze/1075. 나누기/나누기.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int N = sc.nextInt();
+        int F = sc.nextInt();
+        int temp = (N/100)*100;
+        while (true){
+            if (temp%F==0){
+                int result = temp %100;
+           if (result<10){
+               System.out.println("0"+result);
+           }
+           else{
+               System.out.println(result);
+           }
+           return;
+
+            }
+            temp++;
+
+        }
+
+    }
+}
+
+
+
